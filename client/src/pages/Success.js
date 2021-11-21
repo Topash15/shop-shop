8223e4949cfd29146304bc9f22f08098a078c58d
Topash15 <Alextopash@gmail.com>
2021-11-21 10:47:41 -0500

    fixed success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useMutation } from 'react';
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/react-hooks'
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise} from '../utils/helpers';
